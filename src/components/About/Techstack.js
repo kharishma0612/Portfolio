f8571a7e7e951c0862ca771350d92859c5a6ac1d
Kharishma <kharishma06@gmail.com>
2025-03-14 00:35:35 +0530

    skillset added

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -9,6 +9,9 @@ import {
   DiPython,
   DiGit,
   DiJava,
+  DiMysql,
+  DiHtml5,
+  DiCss3,
 } from "react-icons/di";
 import {
   SiRedis,
@@ -16,23 +19,24 @@ import {
   SiNextdotjs,
   SiSolidity,
   SiPostgresql,
+  SiKotlin,
+  SiDart,
+  SiAndroidstudio,
+  SiFlutter,
 } from "react-icons/si";
 import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiC />
-      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
@@ -44,34 +48,22 @@ function Techstack() {
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiMysql />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiHtml5 />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiCss3 />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiKotlin />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiDart />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiAndroidstudio />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFlutter />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDart />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlutter />
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,33 +1,29 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
-} from "react-icons/si";
+import { SiWindows, SiVisualstudiocode, SiGit, SiVercel, SiStreamlit } from "react-icons/si";
+
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
+        <SiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiStreamlit />
+      </Col>
     </Row>
   );
+  
 }
 
 export default Toolstack;
